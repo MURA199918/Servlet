6d284627d5275b10cb401f48775917e276d8f135
MURA199918 <muralisaranr18@gmail.com>
2021-01-13 19:12:40 +0530

    [Murali]Refactored to validate user name using regex

--- MyFirstServlet/src/main/java/com/first_servlet/LoginServlet.java
+++ MyFirstServlet/src/main/java/com/first_servlet/LoginServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 @WebServlet(
         description = "Login Servlet Testing",
@@ -30,14 +31,24 @@ public class LoginServlet extends HttpServlet {
         //get servlet config init params
         String userID = getServletConfig().getInitParameter("user");
         String password = getServletConfig().getInitParameter("password");
-        if(userID.equals(user) && password.equals(pwd)) {
-            request.setAttribute("user", user);
-            request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
-        }else {
-            RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+
+        String userRegEx = "(^[A-Z]{1}[a-z]{2,}$)";
+
+        if(Pattern.matches(userRegEx, user)) {
+            if(userID.equals(user) && password.equals(pwd)) {
+                request.setAttribute("user", user);
+                request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
+            } else {
+                RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+                PrintWriter out = response.getWriter();
+                out.println("<font color=red>Either user name or password is wrong.<font>");
+                rd.include(request, response);
+            }
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
             PrintWriter out = response.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include((ServletRequest) request, response);
+            out.println("<font color=red>Invalid user name.<font>");
+            rd.include(request, response);
         }
     }
 }
